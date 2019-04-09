f5f327b81278cdc6955aa1c26525eefd2c947379
Elise Phan <aliceconnic@gmail.com>
2019-04-09 19:57:04 +0000

    Automatically backed up by Learn

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // are we missing an import?
-import Comment from ./comment.js
-
+import Comment from './comment.js'
+import BlogPost from './BlogPost.js'
 export default class BlogPost extends Component {
   render() {
     
