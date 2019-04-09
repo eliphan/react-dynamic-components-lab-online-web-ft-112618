c4ece6829cfcd295af85bc6ffd1bb9b213ebe10b
Elise Phan <aliceconnic@gmail.com>
2019-04-09 20:05:35 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import BlogPost from './BlogPost.js';
 import ColorBox from './ColorBox.js';
 
-export default class Comment extends Component {
+ extends Component {
   render () {
     return  (
       <div className="comment">
